49e33b0be09c08ffb94155d20218e8f034704bea
Luiz Fernando <devfernandoof@gmail.com>
2020-09-20 19:22:09 -0300

    🚀 create routes.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-app.post("/", (req, res) => {});
+app.use(routes);
 
 app.listen(3333);
