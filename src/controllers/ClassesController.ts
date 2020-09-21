1cdbd4897d9bebcf0f87fb247d8ecd5d32e3d0bd
Luiz Fernando <devfernandoof@gmail.com>
2020-09-21 17:59:18 -0300

    🚀 classes controller

---
+++ src/controllers/ClassesController.ts
@@ -0,0 +1,78 @@
+import { Request, Response } from "express";
+
+import db from "../database/connection";
+import convertHourToMinutes from "../database/utils/convertHourToMinutes";
+
+interface ScheduleItem {
+  week_day: number;
+  from: string;
+  to: string;
+}
+
+export default class ClassesController {
+  async index(req: Request, res: Response) {
+    const filters = req.query;
+
+    const subject = filters.subject as string;
+    const week_day = filters.subject as string;
+    const time = filters.time as string;
+
+    if (!filters.subject || !filters.week_day || !filters.time) {
+      return res.status(400).json({
+        error: "Missing filters to search classes"
+      });
+    }
+
+    const timeInMinutes = convertHourToMinutes(time);
+
+    const classes = await db("classes").where("classes.subject", "=", subject);
+
+    return res.json(classes);
+  }
+
+  async create(req: Request, res: Response) {
+    const { name, avatar, whatsapp, bio, subject, cost, schedule } = req.body;
+
+    const trx = await db.transaction();
+
+    try {
+      const insertedUsersIds = await trx("users").insert({
+        name,
+        avatar,
+        whatsapp,
+        bio
+      });
+
+      const user_id = insertedUsersIds[0];
+
+      const insertedClassesIds = await trx("classes").insert({
+        subject,
+        cost,
+        user_id
+      });
+
+      const class_id = insertedClassesIds[0];
+
+      const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+        return {
+          class_id,
+          week_day: scheduleItem.week_day,
+          from: convertHourToMinutes(scheduleItem.from),
+          to: convertHourToMinutes(scheduleItem.to)
+        };
+      });
+
+      await trx("class_schedule").insert(classSchedule);
+
+      await trx.commit();
+
+      return res.status(201).send();
+    } catch (err) {
+      await trx.rollback();
+
+      return res.status(400).json({
+        error: "Unexpected error while creating new class"
+      });
+    }
+  }
+}
